90b28a9f0c219d29e3705cbe4d4896077decaaae
zhzj <amadeuszhang@outlook.com>
2022-04-05 21:10:32 +0200

    closed es13

--- Esercizi01/13-ScompQuad.py
+++ Esercizi01/13-ScompQuad.py
@@ -7,24 +7,37 @@ Se sì, stampare a video la scomposizione.
 Esempi:
 2 ==> 2 = 1 + 1 = 1^2 + 1^2
 28 ==> NON SCOMPONIBILE
-145 ==> 146 = 25 + 121 = 5^2 + 11^2
-
+146 ==> 146 = 25 + 121 = 5^2 + 11^2
 '''
 
 def main() :
     n = int(input("Inserisci un numero: "))
+    radici = []
+    flag = False
     for i in range(1, n) :
         for j in range(1, n) :
             if i**2 + j**2 == n :
-                print(n, "=", i, "^2 +", j, "^2")
-                return
+                flag = True
+                radici.append((i, j))
     
-    print("NON SCOMPONIBILE")
+    if flag == True :
+        # compare values in radici if there are any coincidence
+        partialsum = []
+        for i in range(len(radici)) :
+            # ogni elemento ha dim 2 :
+            ps = (radici[i][0] + radici[i][1])
+            if ps not in partialsum :
+                partialsum.append(ps)
+                print("{} = {} + {}".format(n, radici[i][0], radici[i][1]))
+    
+    else :
+        print("NON SCOMPONIBILE")
 
 main() # chiamata della funzione principale
 
 '''
-Variante: mostrare tutte le possibili scomposizioni
+Variante: mostrare tutte le possibili scomposizioni [done]
 
 e.g. 50
+
 '''
